658e62c642e23154d0f19338d8cd9aed598f3ca6
Michał Pawlikowski <michalpw159@gmail.com>
2018-04-15 23:55:30 +0200

    Revert "mvvm pattern applied"
    This reverts commit b17f0158616dbc1a1cdd9af22a6e3ac4dabc8137.

---
+++ GeosEnterprise/ViewModels/ComputersListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeosEnterprise.DBO;
+using GeosEnterprise.DTO;
+using GeosEnterprise.Repositories;
+
+namespace GeosEnterprise.ViewModels
+{
+    public class ComputersListViewModel
+    {
+        public ComputersListViewModel()
+        {
+            RepairsRepository.GetAllCurrent();
+        }
+
+        public List<RepairDTO> Items
+        {
+            get
+            {
+                return Repositories.RepairsRepository.GetAllCurrent().Select(p => DTO.RepairDTO.ToDTO(p)).ToList();
+            }
+        }
+    }
+}
