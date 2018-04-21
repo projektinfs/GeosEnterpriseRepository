ad3eda3ad414c6614ffa903d08c4ecaa5eecfd65
Michał Pawlikowski <michalpw159@gmail.com>
2018-04-21 20:43:00 +0200

    Update method in base repository

--- GeosEnterprise/DTO/RepairDTO.cs
+++ GeosEnterprise/DTO/RepairDTO.cs
@@ -10,6 +10,7 @@ namespace GeosEnterprise.DTO
     {
         public int ID { get; set; }
         public ComputerDTO Computer { get; set; }
+        public int ComputerID { get; set; }
         public string Description { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? RealizationDate { get; set; }
@@ -25,6 +26,7 @@ namespace GeosEnterprise.DTO
                     SerialNumber = entity.Computer.SerialNumber,
                     Name = entity.Computer.Name
                 },
+                ComputerID = entity.ComputerID,
                 CreatedDate = entity.CreatedDate,
                 Description = entity.Description,
                 ID = entity.ID,
