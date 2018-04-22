1c4ee0b605cec3dc179cacc659fbedff2161da82
Jaszek <divinitas3@gmail.com>
2018-04-22 17:14:16 +0200

    Adding admin when he not exist

--- GeosEnterprise/Authorization.cs
+++ GeosEnterprise/Authorization.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeosEnterprise.DBO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace GeosEnterprise
     {
 
         public static string _AcctualUser = "NaN";
+        public static Employee AcctualEmploee;
+
         public static string AcctualUser
         {
             get
