857130b84367e945817aab9b60c1174283ffd8c3
Magdalena Hylinska <magdalenahylinska@gmail.com>
2018-05-10 13:35:07 +0200

    Employee validation - phone

--- GeosEnterprise/Validators/EmployeeValidator.cs
+++ GeosEnterprise/Validators/EmployeeValidator.cs
@@ -17,8 +17,8 @@ namespace GeosEnterprise.Validators
             RuleFor(employee => employee.Surname).NotEmpty().WithMessage("Pole Nazwisko nie może być puste");
             RuleFor(employee => employee.Position).NotEmpty().WithMessage("Pole Stanowisko nie może być puste");
             RuleFor(employee => employee.Adress.City).NotEmpty().WithMessage("Pole Miasto nie może być puste");
-            RuleFor(employee => employee.EmployeeContact.Phone).NotEmpty().WithMessage("Pole Telefon nie może być puste");
             RuleFor(employee => employee.Email).NotEmpty().WithMessage("Pole Email nie może być puste").EmailAddress().WithMessage("Nieprawidłowy adres email!");
+            RuleFor(employee => employee.EmployeeContact.Phone).Must(GoodPhone).WithMessage("Podany numer telefonu jest niepoprawny!");
             RuleFor(employee => employee.Adress.PostCode).Must(GoodPostCode).WithMessage("Nieprawidłowy kod pocztowy!");
             RuleFor(employee => employee.Password).NotEmpty().Must(GoodPassword).WithMessage("Hasło musi zawierć prznajmniej 6 znaków!");
 
@@ -35,6 +35,14 @@ namespace GeosEnterprise.Validators
             if (string.IsNullOrEmpty(postcode)) return false;
             Regex regex = new Regex(@"^[0-9]{2}\-[0-9]{3}$");
             return regex.IsMatch(postcode);
+
+        }
+
+        public bool GoodPhone(string number)
+        {
+            if (string.IsNullOrEmpty(number)) return false;
+            Regex regex = new Regex(@"^(?:(\+[0-9]{2})?[0-9]{9})$");
+            return regex.IsMatch(number);
 
         }
     }
