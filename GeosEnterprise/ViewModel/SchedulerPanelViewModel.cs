5d935b0ff35a71787d5e6c30acaef260211363ca
Dominik Bartoszewski <dominbartoszewski@gmail.com>
2018-05-27 23:38:02 +0200

    Added origin of scheduler

---
+++ GeosEnterprise/ViewModel/SchedulerPanelViewModel.cs
@@ -0,0 +1,84 @@
+﻿using GalaSoft.MvvmLight;
+using GeosEnterprise.Commands;
+using GeosEnterprise.DTO;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace GeosEnterprise.ViewModel
+{
+    public class SchedulerPanelViewModel : ViewModelBase
+    {
+        public SchedulerPanelViewModel()
+        {
+            DateTimeNowButtonCommand = new RelayCommand<object>(Now);
+            ResetButtonCommand = new RelayCommand<object>(Reset);
+            //SearchButtonCommand = new RelayCommand<object>(OnSearch);
+        }
+
+        public ICommand DateTimeNowButtonCommand { get; set; }
+        public ICommand ResetButtonCommand { get; set; }
+        //public ICommand SearchButtonCommand { get; set; }
+
+        private DateTime? timeToBindingItem;
+        public DateTime? TimeToBindingItem
+        {
+            get
+            {
+                return timeToBindingItem;
+            }
+            set
+            {
+                if (timeToBindingItem != value)
+                {
+                    timeToBindingItem = value;
+                    RaisePropertyChanged("TimeToBindingItem");
+                }
+            }
+
+        }
+
+        private DateTime? timeFromBindingItem;
+        public DateTime? TimeFromBindingItem
+        {
+            get
+            {
+                return timeFromBindingItem;
+            }
+            set
+            {
+                if (timeFromBindingItem != value)
+                {
+                    timeFromBindingItem = value;
+                    RaisePropertyChanged("TimeFromBindingItem");
+                }
+            }
+        }
+
+        /*
+        private string _searchString;
+        public string SearchString
+        {
+            get { return _searchString; }
+            set { _searchString = value; RaisePropertyChanged("SearchString"); }
+        }
+        */
+
+        public void Now(object obj)
+        {
+            TimeToBindingItem = DateTime.Now;
+        }
+
+        public void Reset(object obj)
+        {
+            TimeToBindingItem = null;
+            TimeFromBindingItem = null;
+        }
+    }
+}
